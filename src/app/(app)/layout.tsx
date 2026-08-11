ee6fee6dada281fb4d31254acfb255459bb9106e
Umer <l233085@lhr.nu.edu.pk>
2026-08-12 02:10:35 +0500

    feat: add changes in layout.tsx after navbar implementation

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,6 +1,6 @@
 import { Navbar } from '@/components/layout/Navbar';
 import { Footer } from '@/components/layout/Footer';
-import { Providers } from '@/providers';
+import { Providers } from '@/app/providers';
 
 export default function AppLayout({
   children,

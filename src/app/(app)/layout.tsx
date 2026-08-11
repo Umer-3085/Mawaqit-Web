f3c919ac40bba4ee3bdcc097cafefa6f15a44efd
Umer <l233085@lhr.nu.edu.pk>
2026-08-12 02:17:28 +0500

    refactor: remove multiple header and footers from the main / route

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,16 +1,7 @@
-import { Navbar } from '@/components/layout/Navbar';
-import { Footer } from '@/components/layout/Footer';
-
 export default function AppLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <>
-      <Navbar />
-      <div className="flex-1 flex flex-col">{children}</div>
-      <Footer />
-    </>
-  );
+  return <>{children}</>;
 }
